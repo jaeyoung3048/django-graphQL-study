0f3a3334f80c409f75934c6eec1bb68d353cecff
jaeyoung <56381650+jaeyoung3048@users.noreply.github.com>
2021-04-05 03:35:50 +0900

    ADD: jwt encode, decode

---
+++ utils/jwt.py
@@ -0,0 +1,16 @@
+import jwt
+
+from django.conf import settings
+
+
+def encode_jwt(data):
+    return jwt.encode(data, settings.SECRET_KEY, algorithm="HS256").decode("utf-8")
+
+
+def decode_jwt(access_token):
+    return jwt.decode(
+        access_token,
+        settings.SECRET_KEY,
+        algorithms=["HS256"],
+        options={"verify_aud": False},
+    )
