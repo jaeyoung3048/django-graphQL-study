ba46f9b78062cff8735a01476c1813ffc15279ce
jaeyoung <56381650+jaeyoung3048@users.noreply.github.com>
2021-04-05 03:54:08 +0900

    UPDATE: account mutations

--- account/mutations/account.py
+++ account/mutations/account.py
@@ -1,5 +1,7 @@
 import graphene
 
+from datetime import datetime, timedelta
+
 from django.core.exceptions import ValidationError
 from django.contrib.auth.password_validation import validate_password
 from django.db import transaction
@@ -8,6 +10,8 @@ from account.models.user import User
 from account.models.user_profile import Profile
 from account.models.token import Token
 from account.mutations.inputs import UserInput, UserProfileInput
+
+from utils.jwt import encode_jwt
 
 
 class RegisterMutation(graphene.Mutation):
@@ -42,4 +46,45 @@ class SigninMutation(graphene.Mutation):
         user_input = UserInput(required=True)
 
     success = graphene.Boolean()
-    token = graphene.String()
+    access_token = graphene.String()
+    refresh_token = graphene.String()
+
+    @classmethod
+    def mutate(cls, root, info, user_input):
+        try:
+            user_obj = User.objects.get(email=user_input.email)
+            validate_password(user_input, user_obj)
+
+            iat = datetime.now()
+            exp = iat + timedelta(hours=1)
+
+            access_token = encode_jwt({
+                "token_type": "access",
+                "email": user_input.email,
+                "iat": iat.timestamp(),
+                "exp": exp.timestamp(),
+                "permission": {
+                    "is_staff": user_input.is_staff,
+                    "is_superuser": user_input.is_superuser
+                }
+            })
+
+            exp = iat + timedelta(days=10)
+
+            refresh_token = encode_jwt({
+                "token_type": "refresh",
+                "email": user_input.email,
+                "iat": iat.timestamp(),
+                "exp": exp.timestamp(),
+                "permission": {
+                    "is_staff": user_input.is_staff,
+                    "is_superuser": user_input.is_superuser
+                }
+            })
+
+            Token.objects.update(user=user_obj, token=refresh_token)
+
+        except Exception as e:
+            raise ValidationError(e)
+
+        return cls(success=True, access_token=access_token, refresh_token=refresh_token)
