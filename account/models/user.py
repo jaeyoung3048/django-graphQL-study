3b483aadb9032e21ee85925356a425621ce9eb21
jaeyoung <56381650+jaeyoung3048@users.noreply.github.com>
2021-04-05 00:15:51 +0900

    UPDATE: models branch

---
+++ account/models/user.py
@@ -0,0 +1,38 @@
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    uid = models.UUIDField(
+        unique=True,
+        primary_key=True,
+        editable=False,
+        default=uuid.uuid4,
+    )
+
+    email = models.EmailField(
+        unique=True,
+        max_length=128,
+    )
+
+    is_staff = models.BooleanField(
+        default=False
+    )
+
+    is_superuser = models.BooleanField(
+        default=False
+    )
+
+    refresh_token = models.TextField(
+        null=True,
+        default=None,
+    )
+
+    EMAIL_FIELDS = 'email'
+    USERNAME_FIELD = 'email'
+
+    class Meta:
+        db_table = "Account.U"
